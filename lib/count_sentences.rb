34d44ac66e1c145b9ec4da0b11b882b644ca613e
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-26 00:22:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,10 +15,15 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     sentence_array = self.split(/[.?!]/)
     sentence_array.delete_if{|sentence| sentence.empty?}
     sentence_array.length
    
+=======
+    self.count_sentences
+    binding.pry 
+>>>>>>> 3ed5b2d8ea608add7284e36438e83bb1349ed494
   end
 end
 #"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
